158d895c331a48d958615e8e6e87d530fa417246
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-23 12:48:07 -0500

    Create boilerplate in App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App () {
+    return (
+        <div className='game-container'>
+            <h1>Speed Typing Game</h1>
+            <textarea value = "Empty for now"/>
+            <h4>Time Remaining: 0</h4>
+            <button>Start Game</button>
+            <h1>Word Count: </h1>
+        </div>
+    )
+}
+
+export default App
