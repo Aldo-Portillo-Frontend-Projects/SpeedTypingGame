cbf279184619e95411675380e8f174edc4688d75
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-23 12:59:20 -0500

    Create text state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 function App () {
+    const [text, setText] = React.useState("")
+    
+    function handleChange(e) {
+        const {value} = e.target
+        setText(value)
+    }
+
+    console.log(text)
+
     return (
         <div className='game-container'>
             <h1>Speed Typing Game</h1>
-            <textarea value = "Empty for now"/>
+            <textarea value={text} onChange={handleChange}/>
             <h4>Time Remaining: 0</h4>
             <button>Start Game</button>
             <h1>Word Count: </h1>
